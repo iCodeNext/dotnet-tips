bac6176ac0521ec1ce503eee2e00df0cf5e439b8
mohammadKarimi <mha.karimi@gmail.com>
2023-01-23 21:20:11 +0330

    add String Syntax

---
+++ iCodeNext/Net7/StringSyntax/StringSyntaxHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace iCodeNext.Net7;
+
+internal class StringSyntaxHelper
+{
+    public StringSyntaxHelper()
+    {
+        GetAllList("");
+        GetAllListByDate("yyyy");
+    }
+
+    public static void GetAllList([StringSyntax(StringSyntaxAttribute.Regex)] string param)
+    {
+
+    }
+    public static void GetAllListByDate([StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string date)
+    {
+
+    }
+}
--- iCodeNext/Program.cs
+++ iCodeNext/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    
+}
+
+ 
